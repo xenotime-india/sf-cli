9f2cefb65ffb1357e2bbbfb772db5d54206e10f4
sandeepkhoj@gmail.com <sandeep@topcoder.com>
2017-04-06 11:25:47 +0530

    first commit

---
+++ config/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sandeepkumar on 16/01/17.
+ */
+const salesforce = require('./salesforce');
+
+module.exports = {
+  salesforce,
+}
---
+++ config/salesforce.js
@@ -0,0 +1,7 @@
+/**
+ * Created by sandeepkumar on 16/01/17.
+ */
+module.exports = {
+  OAUTH_CLIENT_ID : '',
+  OAUTH_CLIENT_SECRET: '',
+};
